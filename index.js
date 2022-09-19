2ab629dd4815b72608f2115f8d2f07a47ab3eb1c
Vaughn Aubrey <vaubrey7@gmail.com>
2022-09-19 10:48:24 -0600

    fixing readme, added test run

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateReadMe = ({ github, linkedin, email, questions, license }) =>
+const generateReadMe = ({
+    github,
+    linkedin,
+    email,
+    questions,
+    license,
+    title,
+    motivation,
+    project,
+    solve,
+    learn,
+    description,
+    table,
+    installation,
+    usage,
+    images,
+    credits,
+  }) =>
   `# ${(title)}
-      
+
+  
 
   ## Description of Project 
   ${(description)}
@@ -53,7 +71,61 @@ const generateReadMe = ({ github, linkedin, email, questions, license }) =>
   `;
 
 inquirer
-  .prompt([
+  .prompt([{
+      type: 'input',
+      name: 'title',
+      message: 'What is your project title?',
+    },
+    {
+      type: 'input',
+      name: 'motivation',
+      message: 'Where did you get your motivation from?',
+    },
+    {
+      type: 'input',
+      name: 'project',
+      message: 'What is a description of the Project?',
+    },
+    {
+      type: 'input',
+      name: 'solve',
+      message: 'What problem does it solve?',
+    },
+    {
+      type: 'input',
+      name: 'learn',
+      message: 'What did you learn?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Add a detailed description of the Project.',
+    },
+    {
+      type: 'input',
+      name: 'table',
+      message: 'add your table of contents',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the steps required to install your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please explain the pratical application of your app/project? Provide instructions and examples for use. Include screenshots as needed.',
+    },
+    {
+      type: 'input',
+      name: 'images',
+      message: 'Enter your images ![alt text](assets/images/screenshot.png)',
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'List those who need to be credited here including tutorials followed etc.',
+    },
     {
       type: 'input',
       name: 'github',
@@ -65,17 +137,16 @@ inquirer
       message: 'Enter your LinkedIn URL.',
     },
     {
-        type: 'list',
-        message: 'What is your preferred license type. Press space to submit.',
-        name: 'license',
-        choices: ['MIT_License', 'GNU_GPLv3', 'Apache_2'],
-      },
+      type: 'list',
+      message: 'What is your preferred license type. Press space to submit.',
+      name: 'license',
+      choices: ['MIT_License', 'GNU_GPLv3', 'Apache_2'],
+    },
   ])
   .then((answers) => {
     const readMePageContent = generateReadMe(answers);
 
-    fs.writeFile('READ.me', readMePageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created READ.me!')
+    fs.writeFile('READmegen.md', readMePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created READmegen.md!')
     );
   });
-
